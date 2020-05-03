0a0a0b2da9ba5e6e1ab7d1e7797fc239d1151dfc
unknown <jpkch2910@gmail.com>
2020-05-03 16:46:07 +0530

    init App

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Jayaprakash\Desktop\django_projects\firstSite\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
